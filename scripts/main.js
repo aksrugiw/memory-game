1fb85df2ad66b56692ab79012d63a8df14ba3230
aksrugiw <kwigurskabak@gmail.com>
2020-07-01 20:17:57 +0200

    Implement Game Board

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
+import { GameBoard } from './game-board.js';
+
 function main() {
-  const cards = document.querySelectorAll('.card');
   const count = 0;
+  const container = document.querySelector('.main');
+  const gameBoard = new GameBoard({ container, size: 5 });
+  gameBoard.render();
+  const cards = document.querySelectorAll('.card');
   const memoryGame = new MemoryGame({ cards, count });
   memoryGame.setupClickEvents();
 }
