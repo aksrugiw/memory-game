58736aef47c6ab80f1dc49ecaf9cf0aa6f0196ec
aksrugiw <kwigurskabak@gmail.com>
2020-07-01 20:15:35 +0200

    Refactor

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,52 @@
-export { ClassName } from './class-name.js';
-
 function main() {
   const cards = document.querySelectorAll('.card');
+  const count = 0;
+  const memoryGame = new MemoryGame({ cards, count });
+  memoryGame.setupClickEvents();
+}
 
-  cards.forEach((card) => {
-    card.addEventListener('click', flipCard);
-  });
+class MemoryGame {
+  constructor(settings) {
+    this.config = {};
 
-  function flipCard() {
-    this.classList.add('card-flip');
+    Object.assign(this.config, settings);
+  }
+
+  setupClickEvents() {
+    const self = this;
+    this.config.cards.forEach((card) => {
+      card.addEventListener('click', (e) => self.flipCard(e));
+    });
+  }
+
+  flipCard(e) {
+    this.config.count++;
+    e.target.classList.add('card-flip');
     const flippedCards = document.querySelectorAll('.card-flip');
-    if (flippedCards.length > 1) {
-      setTimeout(hideCards, 2000, flippedCards);
+    if (this.config.count > 1) {
+      this.disableCards();
+      setTimeout(this.hideCards.bind(this), 1000, flippedCards);
     }
   }
 
-  function hideCards(flippedCards) {
+  disableCards() {
+    this.config.cards.forEach((card) => {
+      card.classList.add('disabled');
+    });
+  }
+
+  enableCards() {
+    this.config.cards.forEach((card) => {
+      card.classList.remove('disabled');
+    });
+  }
+
+  hideCards(flippedCards) {
     flippedCards.forEach((flippedCard) => {
       flippedCard.classList.remove('card-flip');
     });
+    this.config.count = 0;
+    this.enableCards();
   }
 }
 
